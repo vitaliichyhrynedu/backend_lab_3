084aec902f76bec8421c4d5a27512d9ac162bd73
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-10-19 00:17:18 +0300

    Implement the categories module

--- src/categories.rs
+++ src/categories.rs
@@ -8,6 +8,12 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+pub fn router() -> Router<Database<Category>> {
+    Router::new()
+        .route("/", get(get_categories).post(create_category))
+        .route("/{category_id}", get(get_category).delete(delete_category))
+}
+
 #[derive(Clone, Serialize)]
 pub struct Category {
     id: Uuid,
@@ -19,29 +25,35 @@ pub struct NewCategory {
     name: String,
 }
 
-pub fn router() -> Router<Database<Category>> {
-    Router::new()
-        .route("/", get(get_categories).post(create_category))
-        .route("/{category_id}", get(get_category).delete(delete_category))
-}
-
 async fn get_category(
     State(db): State<Database<Category>>,
     Path(id): Path<Uuid>,
 ) -> Json<Category> {
-    todo!()
+    let db = db.read().await;
+    let category = db.get(&id).unwrap().clone();
+    Json(category)
 }
 
 async fn create_category(
     State(db): State<Database<Category>>,
     Json(category): Json<NewCategory>,
 ) -> (StatusCode, Json<Category>) {
-    todo!()
+    let mut db = db.write().await;
+    let category = Category {
+        id: Uuid::new_v4(),
+        name: category.name,
+    };
+    db.insert(category.id, category.clone());
+    (StatusCode::CREATED, Json(category))
 }
+
 async fn delete_category(State(db): State<Database<Category>>, Path(id): Path<Uuid>) -> StatusCode {
-    todo!()
+    let mut db = db.write().await;
+    db.remove(&id);
+    StatusCode::NO_CONTENT
 }
 
 async fn get_categories(State(db): State<Database<Category>>) -> Json<Vec<Category>> {
-    todo!()
+    let db = db.read().await;
+    Json(db.values().cloned().collect())
 }
--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,13 @@ async fn main() {
     let user_database = database();
     let user_router = users::router().with_state(user_database);
 
+    let category_database = database();
+    let category_router = categories::router().with_state(category_database);
+
     let router = Router::new()
         .route("/", get(root))
-        .nest("/users", user_router);
+        .nest("/users", user_router)
+        .nest("/categories", category_router);
 
     let port = env::var("PORT").expect("PORT environment variable must be set");
     let addr = format!("0.0.0.0:{}", port);
