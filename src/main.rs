60b2843fe09cf89a208e6a365d3087dab1451b9b
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-10-18 17:58:42 +0300

    Add initial application setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(root));
+
+    let port = env::var("PORT").expect("PORT environment variable must be set");
+    let addr = format!("0.0.0.0:{}", port);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Welcome to the expense tracker!"
 }
