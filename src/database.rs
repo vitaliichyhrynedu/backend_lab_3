27cec3c0d91921c3f9f12785c1a4feca43afd18e
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-10-25 19:26:56 +0300

    Implement creating a database connection

--- src/database.rs
+++ src/database.rs
@@ -1,10 +1,16 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{env, error::Error};
 
-use tokio::sync::RwLock;
-use uuid::Uuid;
+use sea_orm::{Database, DatabaseConnection};
 
-pub type Database<T> = Arc<RwLock<HashMap<Uuid, T>>>;
+pub async fn create_connection() -> Result<DatabaseConnection, Box<dyn Error>> {
+    let db_host = env::var("DB_HOST")?;
+    let db_user = env::var("DB_USER")?;
+    let db_pass = env::var("DB_PASS")?;
+    let db_port = env::var("DB_PORT")?;
 
-pub fn database<T>() -> Database<T> {
-    Arc::new(RwLock::new(HashMap::new()))
+    let db_url = format!("postgres://{db_user}:{db_pass}@{db_host}:{db_port}/{db_user}");
+
+    let db = Database::connect(db_url).await?;
+
+    Ok(db)
 }
