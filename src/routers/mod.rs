530ea8735e51b3721f262d61b17be9dd3a8d5d0d
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-10-27 05:01:41 +0200

    Refactor the 'records' router to use ORM, handle errors and validate input

--- src/routers/mod.rs
+++ src/routers/mod.rs
@@ -4,21 +4,21 @@ use crate::AppState;
 
 pub mod categories;
 pub mod health;
-// pub mod records;
+pub mod records;
 pub mod users;
 
 pub fn router() -> Router<AppState> {
     let health_router = health::router();
     let user_router = users::router();
     let category_router = categories::router();
-    // let record_router = records::router();
+    let record_router = records::router();
 
     let router = Router::new()
         .route("/", get(root))
         .nest("/health", health_router)
         .nest("/users", user_router)
-        .nest("/categories", category_router);
-    // .nest("/records", record_router)
+        .nest("/categories", category_router)
+        .nest("/records", record_router);
 
     router
 }
--- src/routers/records.rs
+++ src/routers/records.rs
@@ -1,21 +1,35 @@
-use std::fmt::Display;
-
-use crate::database::Database;
 use axum::{
     Json, Router,
     extract::{Path, Query, State},
     http::StatusCode,
-    response::IntoResponse,
     routing::get,
 };
-use chrono::{NaiveDateTime, Utc};
+use chrono::NaiveDateTime;
+use entity::{category, record, user};
+use rust_decimal::Decimal;
+use sea_orm::{
+    ActiveModelTrait, ActiveValue::Set, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter,
+};
 use serde::{Deserialize, Serialize};
+use tokio::try_join;
 use uuid::Uuid;
 
-pub fn router() -> Router<Database<Record>> {
+use crate::{AppState, error::AppError};
+
+pub fn router() -> Router<AppState> {
     Router::new()
         .route("/", get(get_records).post(create_record))
         .route("/{record_id}", get(get_record).delete(delete_record))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RecordBody<T> {
+    record: T,
+}
+
+#[derive(Debug, Serialize)]
+struct RecordsBody<T> {
+    records: Vec<T>,
 }
 
 #[derive(Clone, Serialize)]
@@ -23,15 +37,56 @@ pub struct Record {
     id: Uuid,
     user_id: Uuid,
     category_id: Uuid,
-    datetime: NaiveDateTime,
-    sum: f64,
+    created_at: NaiveDateTime,
+    sum: Decimal,
+}
+
+impl From<record::Model> for Record {
+    fn from(value: record::Model) -> Self {
+        Self {
+            id: value.id,
+            user_id: value.user_id,
+            category_id: value.category_id,
+            created_at: value.created_at,
+            sum: value.sum,
+        }
+    }
 }
 
 #[derive(Deserialize)]
-pub struct NewRecord {
+pub struct RecordCreate {
     user_id: Uuid,
     category_id: Uuid,
-    sum: f64,
+    sum: Decimal,
+}
+
+impl RecordCreate {
+    async fn validate(&self, db: &DatabaseConnection) -> Result<(), AppError> {
+        let mut errors = Vec::new();
+
+        let (user, category) = try_join!(
+            user::Entity::find_by_id(self.user_id).one(db),
+            category::Entity::find_by_id(self.category_id).one(db)
+        )?;
+
+        if user.is_none() {
+            errors.push(("user_id", "user doesn't exist"));
+        }
+
+        if category.is_none() {
+            errors.push(("category_id", "category doesn't exist"));
+        }
+
+        if self.sum <= Decimal::ZERO {
+            errors.push(("sum", "sum is not positive"));
+        }
+
+        if errors.is_empty() {
+            Ok(())
+        } else {
+            Err(AppError::unprocessable_entity(errors))
+        }
+    }
 }
 
 #[derive(Deserialize)]
@@ -40,83 +95,55 @@ pub struct RecordFilterParams {
     category_id: Option<Uuid>,
 }
 
-enum RecordError {
-    MissingFilterParams,
-}
-
-impl Display for RecordError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            RecordError::MissingFilterParams => {
-                write!(f, "At least one filter parameter must be provided")
-            }
-        }
-    }
-}
-
-impl IntoResponse for RecordError {
-    fn into_response(self) -> axum::response::Response {
-        #[derive(Serialize)]
-        struct ErrorResponse {
-            message: String,
-        }
-
-        let status = match self {
-            RecordError::MissingFilterParams => StatusCode::BAD_REQUEST,
-        };
-
-        let body = Json(ErrorResponse {
-            message: self.to_string(),
-        });
-
-        (status, body).into_response()
-    }
-}
-
-async fn get_record(State(db): State<Database<Record>>, Path(id): Path<Uuid>) -> Json<Record> {
-    let db = db.read().await;
-    let record = db.get(&id).unwrap().clone();
-    Json(record)
+async fn get_record(
+    State(AppState { db }): State<AppState>,
+    Path(id): Path<Uuid>,
+) -> Result<Json<RecordBody<Record>>, AppError> {
+    let record = record::Entity::find_by_id(id)
+        .one(&db)
+        .await?
+        .ok_or(AppError::NotFound)?
+        .into();
+    Ok(Json(RecordBody { record }))
 }
 
 async fn create_record(
-    State(db): State<Database<Record>>,
-    Json(record): Json<NewRecord>,
-) -> (StatusCode, Json<Record>) {
-    let mut db = db.write().await;
-    let record = Record {
-        id: Uuid::new_v4(),
-        user_id: record.user_id,
-        category_id: record.category_id,
-        datetime: Utc::now().naive_utc(),
-        sum: record.sum,
+    State(AppState { db }): State<AppState>,
+    Json(body): Json<RecordBody<RecordCreate>>,
+) -> Result<(StatusCode, Json<RecordBody<Record>>), AppError> {
+    body.record.validate(&db).await?;
+    let record = record::ActiveModel {
+        user_id: Set(body.record.user_id),
+        category_id: Set(body.record.category_id),
+        sum: Set(body.record.sum),
+        ..Default::default()
     };
-    db.insert(record.id, record.clone());
-    (StatusCode::CREATED, Json(record))
+    let record = record.insert(&db).await?.into();
+    Ok((StatusCode::CREATED, Json(RecordBody { record })))
 }
 
-async fn delete_record(State(db): State<Database<Record>>, Path(id): Path<Uuid>) -> StatusCode {
-    let mut db = db.write().await;
-    db.remove(&id);
-    StatusCode::NO_CONTENT
+async fn delete_record(
+    State(AppState { db }): State<AppState>,
+    Path(id): Path<Uuid>,
+) -> Result<StatusCode, AppError> {
+    let res = record::Entity::delete_by_id(id).exec(&db).await?;
+    match res.rows_affected {
+        0 => Err(AppError::NotFound),
+        _ => Ok(StatusCode::NO_CONTENT),
+    }
 }
 
 async fn get_records(
-    State(db): State<Database<Record>>,
+    State(AppState { db }): State<AppState>,
     Query(params): Query<RecordFilterParams>,
-) -> Result<Json<Vec<Record>>, RecordError> {
-    let (user_id, category_id) = (params.user_id, params.category_id);
-    if user_id.is_none() && category_id.is_none() {
-        return Err(RecordError::MissingFilterParams);
+) -> Result<Json<RecordsBody<Record>>, AppError> {
+    let mut query = record::Entity::find();
+    if let Some(user_id) = params.user_id {
+        query = query.filter(record::Column::UserId.eq(user_id));
     }
-
-    let db = db.read().await;
-    let records = db
-        .values()
-        .filter(|record| user_id.map_or(true, |user_id| record.user_id == user_id))
-        .filter(|record| category_id.map_or(true, |category_id| record.category_id == category_id))
-        .cloned()
-        .collect();
-
-    Ok(Json(records))
+    if let Some(category_id) = params.category_id {
+        query = query.filter(record::Column::CategoryId.eq(category_id));
+    }
+    let records = query.all(&db).await?.into_iter().map(Into::into).collect();
+    Ok(Json(RecordsBody { records }))
 }
